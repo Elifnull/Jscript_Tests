a7e082a583477e2862f41428a1e73e55485f9545
elifnull <Elifnull@protonmail.com>
2025-04-13 21:44:13 -0400

    switch case added instead of if

--- jscript.js
+++ jscript.js
@@ -1,25 +1,55 @@
 function getComputerChoice() {
     let choice = Math.floor((Math.random()*3)+1);
     let outcome 
+    let user = "CPU"
      if (choice == 1){
-        outcome = "Rock"
+        outcome = ["Rock", user, -1];
      } else if (choice == 2){
-        outcome = "Paper"
+        outcome = ["Paper", user, 1]
      } else if (choice == 3){
-        outcome = "Scissors"
+        outcome = ["Scissors", user, 0];
      }
      return outcome;
 };
 
-let userChoice = prompt("Which do you chose Rock, Paper, or Scissors?")
+let cpuChoice = getComputerChoice();
+let userChoice = prompt("Which do you chose Rock, Paper, or Scissors?");
+let player = "you";
 
-switch (userChoice.toLowerCase()){
-    case 
+
+switch (userChoice.toLowerCase().charAt(0)){
+    case "r":
+        userChoice = ["Rock", player, -1];
+    break;
+
+    case "p":
+        userChoice = ["Paper", player, 1];
+    break;
+
+    case "s":
+        userChoice = ["Scissors", player, 0];
+    break;
 }
-console.log(getComputerChoice());
-console.log(typeof(userChoice));
 
-function evaluateChoise(userChoice,getComputerChoice){
-    //notes
+
+
+console.log(`CPU Chose: ${cpuChoice[0]} \nUser Chose: ${userChoice[0]}`);
+
+function evaluateChoices(){
+    let arrayEvaluation = cpuChoice[2] + userChoice[2];
+    console.log( `Cpu: ${cpuChoice}\nPlayer: ${userChoice}`);
+    console.log(arrayEvaluation);
+
+    if (arrayEvaluation == -1) {
+        alert(`Player Chose: ${userChoice[0]}\nCPU Chose: ${cpuChoice[0]}. \nWinner is Rock`);
+    } else if (arrayEvaluation == 0){
+        alert(`Player Chose: ${userChoice[0]}\nCPU Chose: ${cpuChoice[0]}. \nWinner is Paper`);
+    } else if (arrayEvaluation == 1){
+        alert(`Player Chose: ${userChoice[0]}\nCPU Chose: ${cpuChoice[0]}. \nWinner is Scissors`);
+    } else {
+        alert(`Player Chose: ${userChoice[0]}\nCPU Chose: ${cpuChoice[0]}. \nThis is a Tie`);
+    }
 };
+
+evaluateChoices();
 
