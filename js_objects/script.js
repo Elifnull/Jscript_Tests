a17f4e2c7514565a91b28e8595eae69b1d7926c6
MaxwellPro <schvorun@gmail.com>
2025-05-23 17:38:50 -0400

    reduce method exploration

--- js_objects/script.js
+++ js_objects/script.js
@@ -48,3 +48,80 @@ function multiplyNumByTwo(obj){
         }
     }
 }
+
+
+/* 
+Array methods
+*/
+
+//Maping
+
+/*
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let users = [ john, pete, mary ];
+
+let names = users.map(item => item.name);
+
+alert( names ); // John, Pete, Mary
+
+*/
+/*
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
+
+let users = [ john, pete, mary ];
+
+let usersMapped = users.map(user =>( 
+    {fullName:`${user.name} ${user.surname}`, 
+    id:`${user.id}`}));
+
+/*
+usersMapped = [
+  { fullName: "John Smith", id: 1 },
+  { fullName: "Pete Hunt", id: 2 },
+  { fullName: "Mary Key", id: 3 }
+]
+
+alert( usersMapped[0].id ) // 1
+alert( usersMapped[0].fullName ) // John Smith
+
+
+arr.sort( (a, b) => a - b );
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let arr = [ pete, john, mary ];
+
+function sortByAge(array){
+    array.sort((a, b) => (a.age - b.age));
+};
+
+sortByAge(arr);
+
+
+
+// now: [john, mary, pete]
+alert(arr[0].name); // John
+alert(arr[1].name); // Mary
+alert(arr[2].name); // Pete
+*/
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 29 };
+
+let arr = [ john, pete, mary ];
+
+
+function getAverageAge(arr){
+    let age =arr.reduce((sum, current)=> 1+1);
+};
+
+alert( getAverageAge(arr) );
+
